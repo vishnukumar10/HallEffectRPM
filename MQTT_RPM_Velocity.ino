5dc1cfaab38196d5e8f02477992163f9e5687cfc
Vishnu Kumar V H <vishnukumarvh10@gmail.com>
2019-04-09 10:46:25 +0530

    Update and rename MQTT_RPM.ino to MQTT_RPM_Velocity.ino

--- MQTT_RPM_Velocity.ino
+++ MQTT_RPM_Velocity.ino
@@ -1,14 +1,36 @@
 #include <ESP8266WiFi.h>
-
-int hallsensor = 2;                // Hall sensor at pin 2
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 
 volatile byte counter;
 
 unsigned int rpm;
-unsigned int v;
+unsigned int v =0;
 
 unsigned long passedtime;
 
+
+#define WLAN_SSID       "****"
+#define WLAN_PASS       "****"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "****"
+#define AIO_KEY         "****"
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish Rpm = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/rpm");
+Adafruit_MQTT_Publish Velocity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/velocity");
+// Setup feeds subscribing to changes.
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
 
 void isr()
 
@@ -23,51 +45,10 @@ void isr()
       counter++;
 
  }
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+void setup()
 
-/************************* WiFi Access Point *********************************/
-
-#define WLAN_SSID       "SSID"
-#define WLAN_PASS       "Password"
-
-/************************* Adafruit.io Setup *********************************/
-
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "USERNAME"
-#define AIO_KEY         "AIO KEY"
-
-/************ Global State (you don't need to change this!) ******************/
-
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
-// or... use WiFiFlientSecure for SSL
-//WiFiClientSecure client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-/****************************** Feeds ***************************************/
-
-// Setup feeds for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish Rpm = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/rpm");
-Adafruit_MQTT_Publish Velocity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/velocity");
-// Setup feeds subscribing to changes.
-
-/*************************** Sketch Code ************************************/
-
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
-void MQTT_connect();
-
-void setup() {
-  Serial.begin(115200);
- 
-  delay(10);
-pinMode(D2,OUTPUT);
-  // Connect to WiFi access point.
+ {
+  Serial.begin(9600);
   Serial.println(); Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
@@ -81,28 +62,30 @@ pinMode(D2,OUTPUT);
 
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+   //Intiates Serial communications
+
    attachInterrupt(0, isr, RISING); //Interrupts are called on Rise of Input
 
-   pinMode(hallsensor, INPUT); //Sets hallsensor as input
 
    counter = 0;
 
-   rpm = 0;
+   rpm = 0;   
 
    passedtime = 0; //Initialise the values
 
+ }
+ uint32_t x=0;
+
+ void loop()
+ {
+    MQTT_connect();
+     Serial.print(F("\nSending RPM and Velocity Values "));
+ 
   
- 
-}
+   delay(5000);//Update RPM every second
 
-uint32_t x=0;
-
-void loop() {
- 
-  MQTT_connect();
- 
- // Now we can publish stuff!
-  detachInterrupt(0); //Interrupts are disabled
+   detachInterrupt(0); //Interrupts are disabled
 
    rpm = 30*1000/(millis() - passedtime)*counter;
 
@@ -110,16 +93,21 @@ void loop() {
 
    counter = 0;
 
-   v=0;
+   /* Velocity = RPM * 3.14 / Diameter of the Wheel 
+
+   Assuming the Diameter of the wheel is 0.75m and conversion for 
+
+   m/s to km/h */
+
+  v = rpm * 3.14 * 3.6 / 75; 
 
    Serial.print("RPM=");
 
-   Serial.println(rpm); //Print out result to monitor
-
-   
-  Serial.print(F("\nSending RPM and Velocity Values "));
- 
-  if (! Rpm.publish(rpm)) {
+   Serial.println(rpm);
+     Serial.print("Velocity=");
+//
+  Serial.println(v); //Print out result to monitor
+    if (! Rpm.publish(rpm)) {
     Serial.println(F("Failed"));
   } else {
     Serial.println(F("OK!"));
@@ -128,22 +116,15 @@ void loop() {
     Serial.println(F("Failed"));
   } else {
     Serial.println(F("OK!"));
-  }
-  delay(1000);
+  }//Print out result to monitor
 
-  // ping the server to keep the mqtt connection alive
-  // NOT required if you are publishing once every KEEPALIVE seconds
-  /*
-  if(! mqtt.ping()) {
-    mqtt.disconnect();
-  }
-  */
-  attachInterrupt(0, isr, RISING);   //Restart the interrupt processing
-}
 
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
+   
+   attachInterrupt(0, isr, RISING);   //Restart the interrupt processing
+
+  }
+
+  void MQTT_connect() {
   int8_t ret;
 
   // Stop if already connected.
